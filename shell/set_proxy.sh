c2da4f79e5fcbe61dca3acbe0b5295fbdc7e68b7
Swapnil Agarwal <a.swapnil@iitg.ernet.in>
2013-10-05 21:05:38 +0530

    Added set_proxy.sh
    set_proxy.sh automates setting up proxy

---
+++ shell/set_proxy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# @Script set_proxy.sh
+# @Author Swapnil Agarwal
+# @Purpose To automate setting up proxy
+# @Usage ./set_proxy.sh
+# Copyright (c) 2013 Swapnil Agarwal
+# Permission to copy and modify is granted under MIT License
+
+echo When any file opens, paste the clipboard content and save before closing gedit.
+echo -n Enter proxy:
+read proxy
+echo -n Enter port:
+read port
+echo -n Enter username:
+read user
+echo -n Enter password:
+read pass
+p=$user:$pass@$proxy:$port
+
+sudo apt-get install xclip
+
+echo -e -n "export http_proxy=http://$p
+export https_proxy=http://$p
+export ftp_proxy=http://$p
+" | xclip -sel clip
+sudo gedit /etc/bash.bashrc
+
+echo -e -n "Acquire::http::proxy \"http://$p\";
+Acquire::https::proxy \"http://$p\";
+Acquire::ftp::proxy \"http://$p\";
+Acquire::http::No-Cache \"True\";
+" | xclip -sel clip
+sudo gedit /etc/apt/apt.conf
+
+echo -e -n "http_proxy=http://$p
+https_proxy=https://$p
+ftp_proxy=ftp://$p
+" | xclip -sel clip
+sudo gedit /etc/wgetrc
