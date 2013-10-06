87915fa5ce8c457241f19eb8f9015fafb8936a97
Swapnil Agarwal <a.swapnil@iitg.ernet.in>
2013-10-06 14:48:58 +0530

    Added macros.cpp
    macros.cpp contains my template which i use while programming

---
+++ c++/macros.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cctype>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <list>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <string>
+#include <sstream>
+#include <cmath>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int,int> PII;
+typedef pair<ULL,ULL> PUU;
+typedef vector<int> VI;
+typedef vector<long> VL;
+typedef vector<string> VS;
+
+#define FOR(i,a,b) for(ULL i=a;i<b;i++)
+#define FORE(i,a,b) for(ULL i=a;i<=b;i++)
+#define FORD(i,a,b) for(ULL i=a;i>=b;i--)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(c) (c).begin(), (c).end()
+#define SORT(c) sort(ALL(c))
+#define PB push_back
+#define MP make_pair
+
+#define INF (int)1e9
+#define EPS 1e-9
+
+int main()
+{
+    std::ios_base::sync_with_stdio (false);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+    #endif
+    cout<<"Hello World..!"<<endl;
+return 0;
+}
