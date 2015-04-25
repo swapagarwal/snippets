2be671d884e6a463c423bb538a3db8a18f7308b2
Swapnil Agarwal <swapnilagarwal@live.com>
2015-04-25 23:59:05 +0530

    Add batch install script

---
+++ shell/batch_install.sh
@@ -0,0 +1,9 @@
+packages=( "xclip" "mplayer" )
+
+for i in "${packages[@]}"
+do
+    if [ $(dpkg-query -W -f='${Status}' $i | grep -c "install ok installed") -eq 0 ];
+    then
+        sudo -E apt-get -y install $i
+    fi
+done
